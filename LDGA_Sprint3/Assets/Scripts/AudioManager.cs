e2c646ecd7b17f43e3d3739f68426672b62c2eb0
JordiCowern <jdcowern@gmail.com>
2025-09-30 22:59:03 -0400

    Arena with phases

--- LDGA_Sprint3/Assets/Scripts/AudioManager.cs
+++ LDGA_Sprint3/Assets/Scripts/AudioManager.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public AudioClip[] ad;
+    public int i = 0;
+
+    private AudioSource aud;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        aud = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1) && i < ad.Length)
+        {
+            aud.clip = ad[i];
+            aud.Play();
+            i++;
+        }
     }
 }
